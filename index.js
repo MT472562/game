438b87ea288acabf8c63acf795bff21a985a82fc
MT472562 <88234127+MT472562@users.noreply.github.com>
2021-09-13 11:29:19 +0900

    ？アラート

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const div = document.getElementById("div");
 const yesButton = document.getElementById("yes");
+const sk = document.getElementById("sk");
 const noButton = document.getElementById("no");
 const audio = new Audio("introduction.mp3");
 
@@ -21,3 +22,8 @@ setTimeout(function(){
     window.location.href = 'inport.html';
     },110*1000);
 });
+sk.addEventListener("click", () => {
+    if (window.confirm("サイトを移動しますか？")) {
+    window.location.href = 'inport.html';
+    }
+    });
